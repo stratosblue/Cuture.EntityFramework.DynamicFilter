22eba17fa24ad7eaff51817a84ff8223793e8add
stratos <stratosblue@outlook.com>
2025-12-06 23:24:51 +0800

    fix: 修正global using导致的编译错误;

---
+++ src/Cuture.EntityFramework.DynamicFilter/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿#if NET10_0_OR_GREATER
+
+global using ParameterValues = System.Collections.Generic.Dictionary<System.String, System.Object?>;
+
+#else
+
+global using ParameterValues = Microsoft.EntityFrameworkCore.Query.Internal.IParameterValues;
+
+#endif
